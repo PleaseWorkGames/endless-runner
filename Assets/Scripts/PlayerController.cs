de649a094e05b19a88b71a24db6ffaa05af2fca8
Dan Mooney <dan.r.mooney@gmail.com>
2018-03-04 10:04:54 -0600

    Add constant velocity right to player sprite

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody2D player;
+	
 	// Use this for initialization
 	void Start () {
-		
+		player = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		player.velocity = transform.right * 10.0f;
 	}
 }
