2657908d27720a4ba9562b8fd39ed54e93af0b35
Dan Mooney <dan.r.mooney@gmail.com>
2018-03-04 10:17:54 -0600

    Add TransformableLeft class

---
+++ Assets/Scripts/TransformableLeft.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformableLeft : MonoBehaviour
+{
+	private Transform transform;
+	
+	// Use this for initialization
+	void Start () {
+//		var curTransform : Transform;
+//		curTransform = gameObject.GetComponent(Transform);		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
