bc25440db496e429e499c7212df6ca4bab77afff
Michael Yu <hyu@magento.com>
2018-03-04 18:25:09 -0600

    Added movement, reset, and death

---
+++ Assets/Scripts/Michael/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public bool hasDied;
+	public int health;
+
+	// Use this for initialization
+	void Start () {
+		hasDied = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.y < -7) {
+			hasDied = true;
+		}
+
+		if (hasDied == true) {
+			StartCoroutine ("Die");
+		}
+	}
+
+	IEnumerator Die () {
+		SceneManager.LoadScene ("Prototype");
+		yield return null;
+	}
+}
