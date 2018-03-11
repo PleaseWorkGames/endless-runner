f70a271e3a9213fc277ef3b641b12b6b57ca44bf
Dan Mooney <dan.r.mooney@gmail.com>
2018-03-11 10:33:17 -0500

    Add comment and format

--- Assets/Scripts/FloorChunkGenerator.cs
+++ Assets/Scripts/FloorChunkGenerator.cs
@@ -15,7 +15,6 @@ public class FloorChunkGenerator : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		boxCollider = gameObject.GetComponent<BoxCollider2D>() as BoxCollider2D;
-		
 	}
 	
 	// Update is called once per frame
@@ -25,16 +24,25 @@ public class FloorChunkGenerator : MonoBehaviour {
 
 	//TODO: Refactor to use tags
 	void OnTriggerExit2D(Collider2D other){
-		if(other.gameObject.GetComponent<Floor>() !=null){
-			Floor newFloor = Instantiate(floor, new Vector2(0,0), transform.rotation,  GetComponentInParent<Transform>());
+		// create new floor as soon as previous floor instance leaves box collider generator 
+		if(other.gameObject.GetComponent<Floor>() != null){
+			Floor newFloor = Instantiate(
+				floor, 
+				new Vector2(0,0), 
+				transform.rotation, 
+				GetComponentInParent<Transform>()
+			);
 
 			float distance = Random.value * maxDistance;
 
 			//Need to create an offset since it's created in the middle of the parent object
 			EdgeCollider2D ec = newFloor.GetComponent<EdgeCollider2D>() as EdgeCollider2D;
-			float offset = Vector2.Distance(ec.points[0], ec.points[ec.points.Length -1])/2;
+			float offset = Vector2.Distance(ec.points[0], ec.points[ec.points.Length -1]) /2;
 
-			newFloor.transform.localPosition = new Vector2(offset - boxCollider.size.x + distance,floor.transform.localPosition.y);
+			newFloor.transform.localPosition = new Vector2(
+				offset - boxCollider.size.x + distance, 
+				floor.transform.localPosition.y
+			);
 
 			newFloor.translatableValue = translatableValue;
 		} 
