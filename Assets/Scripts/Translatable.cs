d3517c8b69a7cd708325c719b23cad34c32c8415
Dan Mooney <dan.r.mooney@gmail.com>
2018-03-11 12:24:03 -0500

    Add clouds/foreground translatable distance type

--- Assets/Scripts/Translatable.cs
+++ Assets/Scripts/Translatable.cs
@@ -7,9 +7,11 @@ public class Translatable : MonoBehaviour
 {
 	private Transform transform;
 	
+	public enum TranslateType {Foreground = 0, Background = 1};
+	
 	public TranslatableValue translateDistance;
 	
-	public enum translatableType {Foreground, Background}
+	public TranslateType translateType = TranslateType.Foreground;
 	
 	// Use this for initialization
 	void Start ()
@@ -20,6 +22,11 @@ public class Translatable : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		transform.Translate(Time.deltaTime * translateDistance.foregroundX, 0, 0);
+		// get translate distance based on the translate type (i.e. foreground, background) assigned to translatable
+		float translateDistanceForThisType = translateType == TranslateType.Foreground 
+			? translateDistance.foregroundX 
+			: translateDistance.backgroundX;
+		
+		transform.Translate(Time.deltaTime * translateDistanceForThisType, 0, 0);
 	}
 }
