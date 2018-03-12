5276d782629ea8c7730ce4416fa4609b14b04b26
Michael Yu <hyu@magento.com>
2018-03-11 19:08:58 -0500

    Added scoreboard and timer

---
+++ Assets/Scripts/Michael/ScoreBoard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour {
+
+	public Text scoreText;
+	public Transform player;
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = "Score: " + (Mathf.RoundToInt(player.position.x) + 7).ToString("0");
+	}
+}
---
+++ Assets/Scripts/Michael/Timer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public Text timeText;
+	public static float time = 0.0f;
+
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		timeText.text = "Time: " + (Mathf.RoundToInt(time)).ToString("0");
+	}
+}
