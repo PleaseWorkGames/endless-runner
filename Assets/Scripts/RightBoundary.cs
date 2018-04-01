c34a54318213a6a0cce70b757ded2fe2bdf4b015
James Calcaben <jcalcaben+github@gmail.com>
2018-04-01 00:20:53 -0500

    [BUGFIX] Prevent player from going past right camera edge

---
+++ Assets/Scripts/RightBoundary.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightBoundary : MonoBehaviour {
+
+	public Camera camera;
+	private EdgeCollider2D edgeCollider2D;
+	// Use this for initialization
+	void Start () {
+		edgeCollider2D = this.GetComponent<EdgeCollider2D>() as EdgeCollider2D;
+
+		if(edgeCollider2D == null) {
+			edgeCollider2D = initEdgeCollider();
+		}
+	}
+
+	private EdgeCollider2D initEdgeCollider() {
+		EdgeCollider2D result = gameObject.AddComponent(typeof(EdgeCollider2D)) as EdgeCollider2D;
+
+		List<Vector2> points = new List<Vector2>();
+
+		//Align the collider with the right camera border
+		Vector2 topRight = camera.ViewportToWorldPoint(new Vector3(1,1,camera.nearClipPlane));
+		Vector2 bottomRight = camera.ViewportToWorldPoint(new Vector3(1,-1,camera.nearClipPlane));
+
+		points.Add(topRight);
+		points.Add(bottomRight);
+
+		result.points = points.ToArray();
+
+
+		return result;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
