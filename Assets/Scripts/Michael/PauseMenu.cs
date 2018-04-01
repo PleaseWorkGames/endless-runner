3ce1191cb495778508730eb5a3037a8324788be9
Michael Yu <hyu@magento.com>
2018-04-01 13:04:04 -0500

    Added pause menu

---
+++ Assets/Scripts/Michael/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PauseMenu : MonoBehaviour {
+
+	public static bool gameIsPaused = false;
+	public GameObject pauseMenuUI;
+	public GameObject pauseButtonUI;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			pauseButton();
+		}	
+	}
+
+	public void pauseButton(){
+		if (gameIsPaused) {
+			Resume();
+		} else {
+			Pause();
+		}
+	} 
+
+	public void Resume(){
+		pauseMenuUI.SetActive(false);
+		pauseButtonUI.SetActive(true);
+		Time.timeScale = 1f;
+		gameIsPaused = false;
+	}
+
+	public void Pause(){
+		pauseMenuUI.SetActive(true);
+		pauseButtonUI.SetActive(false);
+		Time.timeScale = 0f;
+		gameIsPaused = true;
+	}
+		
+	public void Quit(){
+		Application.Quit ();
+	}
+}
