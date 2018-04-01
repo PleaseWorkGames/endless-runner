c565f3b07f9c1972150011b507440d941e64dea0
James Calcaben <jcalcaben+github@gmail.com>
2018-04-01 09:47:14 -0500

    Rename a variable to prevent naming conflict

--- Assets/Scripts/RightBoundary.cs
+++ Assets/Scripts/RightBoundary.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RightBoundary : MonoBehaviour {
 
-	public Camera camera;
+	public Camera myCamera;
 	private EdgeCollider2D edgeCollider2D;
 	// Use this for initialization
 	void Start () {
@@ -21,8 +21,8 @@ public class RightBoundary : MonoBehaviour {
 		List<Vector2> points = new List<Vector2>();
 
 		//Align the collider with the right camera border
-		Vector2 topRight = camera.ViewportToWorldPoint(new Vector3(1,1,camera.nearClipPlane));
-		Vector2 bottomRight = camera.ViewportToWorldPoint(new Vector3(1,-1,camera.nearClipPlane));
+		Vector2 topRight = myCamera.ViewportToWorldPoint(new Vector3(1,1,myCamera.nearClipPlane));
+		Vector2 bottomRight = myCamera.ViewportToWorldPoint(new Vector3(1,-1,myCamera.nearClipPlane));
 
 		points.Add(topRight);
 		points.Add(bottomRight);
