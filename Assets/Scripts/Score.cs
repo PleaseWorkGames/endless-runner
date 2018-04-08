bfc6fdfa52cc0565cc148fdc5308499a9447ddc8
Dan Mooney <dan.r.mooney@gmail.com>
2018-04-08 12:02:38 -0500

    Add multipliers to Score

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -5,6 +5,16 @@ using UnityEngine.UI;
 
 public class Score : TextAbstract
 {
+    [System.Serializable]
+    public class Multiplier
+    {
+        public float leftBound;
+        public float rightBound;
+        public float multiplier = 1.0f;
+    }
+
+    public Multiplier[] Multipliers;
+    
     private float time = 0.0f;
 
     private float score = 0;
