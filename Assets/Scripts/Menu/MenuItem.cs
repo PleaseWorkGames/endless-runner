a9423b266900fc79265604f63b89b6a4c55a9367
James Calcaben <jcalcaben+github@gmail.com>
2018-04-24 22:49:15 -0500

    Add Menu-Prototype scene with basic menu functionality

---
+++ Assets/Scripts/Menu/MenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * The base class for menu items
+ */
+public class MenuItem : MonoBehaviour {
+
+	public void ClickAction(){
+		Debug.Log("Hello World!");
+	}
+}
--- Assets/Scripts/Michael/PauseMenu.cs
+++ Assets/Scripts/Michael/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class PauseMenu : MonoBehaviour {
@@ -9,10 +10,91 @@ public class PauseMenu : MonoBehaviour {
 	public GameObject pauseMenuUI;
 	public GameObject pauseButtonUI;
 
+	public Sprite menuItemSprite;
+
+	private Canvas canvas;
+
+	private GraphicRaycaster raycaster;
+
+	private Component[] menuItems;
+
+	void Start(){
+		//Init and disable the menu UI
+		canvas = gameObject.GetComponent<Canvas>() as Canvas;
+		if(canvas == null){
+			canvas = InitCanvas();
+		}
+		canvas.enabled = false;
+
+		//Init raycaster
+		raycaster = gameObject.GetComponent<GraphicRaycaster>() as GraphicRaycaster;
+		if( raycaster == null ){
+			raycaster = InitRaycaster();
+		}
+
+		menuItems = gameObject.GetComponentsInChildren(typeof(MenuItem));
+
+		foreach(MenuItem item in menuItems){
+			AddMenuItem(item);
+		}
+	}
+
+	private Canvas InitCanvas(){
+		Canvas result = gameObject.AddComponent(typeof(Canvas)) as Canvas;
+
+		result.renderMode = RenderMode.ScreenSpaceOverlay;
+
+		return result;
+	}
+
+	private GraphicRaycaster InitRaycaster() {
+		GraphicRaycaster result = gameObject.AddComponent(typeof(GraphicRaycaster)) as GraphicRaycaster;
+
+		return result;
+	}
+
+	/**
+	 * Create menu item from a GameObject with a MenuItem script attached
+	 */
+	private void AddMenuItem(MenuItem item){
+
+		// Decorate the GameObject associated with the MenuItem script
+		item.gameObject.AddComponent(typeof(CanvasRenderer));
+
+		Image image = item.gameObject.AddComponent(typeof(Image)) as Image;
+		image.type = Image.Type.Sliced;
+		image.sprite = menuItemSprite;
+
+		Button button = item.gameObject.AddComponent(typeof(Button)) as Button;
+
+		// Create a child object for the text in the menu
+		GameObject go = new GameObject("Text");
+		go.transform.parent = item.transform;
+		go.transform.position = button.transform.position;
+
+		Text buttonText = go.AddComponent(typeof(Text)) as Text;
+
+		go.GetComponent<RectTransform>().sizeDelta = item.GetComponent<RectTransform>().sizeDelta;
+
+		buttonText.alignment = TextAnchor.MiddleCenter;
+		buttonText.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+
+		buttonText.text = item.name;
+
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Escape)) {
-			pauseButton();
+			if( !gameIsPaused )
+			{
+				Pause();
+				canvas.enabled = true;
+			}
+			else{
+				Resume();
+				canvas.enabled = false;
+			}
 		}	
 	}
 
@@ -25,15 +107,11 @@ public class PauseMenu : MonoBehaviour {
 	} 
 
 	public void Resume(){
-		pauseMenuUI.SetActive(false);
-		pauseButtonUI.SetActive(true);
 		Time.timeScale = 1f;
 		gameIsPaused = false;
 	}
 
 	public void Pause(){
-		pauseMenuUI.SetActive(true);
-		pauseButtonUI.SetActive(false);
 		Time.timeScale = 0f;
 		gameIsPaused = true;
 	}
